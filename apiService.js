d8d8110a97bac70e51feecada7e9f13a80f72f88
tobiloba <tobiosoba777@gmail.com>
2024-07-13 17:44:48 -0700

    Committed

--- apiService.js
+++ apiService.js
@@ -5,7 +5,8 @@ export const fetchData = async (endpoint) => {
   const resourceUrl = "https://timbu-get-single-product.reavdev.workers.dev/3902678375224b58bee75c5b54ff8d0a"
   const useEndpoint = endpoint === "/" ? resourceUrl : resourceUrl + endpoint;
   try {
-    const urll = `https://timbu-get-all-products.reavdev.workers.dev/3902678375224b58bee75c5b54ff8d0a?organization_id=${import.meta.env.VITE_ORG_ID}&reverse_sort=false&page=1&size=10&Appid=${import.meta.env.VITE_APP_ID}&Apikey=${import.meta.env.VITE_API_KEY}`
+    // const urll = `https://timbu-get-all-products.reavdev.workers.dev/3902678375224b58bee75c5b54ff8d0a?organization_id=${import.meta.env.VITE_ORG_ID}&reverse_sort=false&page=1&size=10&Appid=${import.meta.env.VITE_APP_ID}&Apikey=${import.meta.env.VITE_API_KEY}`
+    const urll = `https://timbu-get-all-products.reavdev.workers.dev/?organization_id=ca7b597cebd049f6bc533f71d63661be&reverse_sort=false&page=1&size=10&Appid=2JKQ8NFUUQSV2LQ&Apikey=bb65a8a2ce1f4ab2b9b2f144bbecc96620240713125415418473`
     const response = await axios.get(urll)
     return response.data;
   } catch (error) {
