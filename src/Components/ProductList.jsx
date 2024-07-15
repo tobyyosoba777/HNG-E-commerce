dc7926b117d891aaeb45f64b634c92dedb9735cb
tobiloba <tobiosoba777@gmail.com>
2024-07-15 15:34:08 -0700

    Updated

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -64,42 +64,25 @@
 import React from 'react';
 import ProductCard from './ProductCard';
 
-const ProductList = ({ products, currentPage, totalPages, onPageChange }) => {
-  const productsPerPage = 10;
-
-  // Calculate the index range of products to display for the current page
-  const startIndex = (currentPage - 1) * productsPerPage;
-  const endIndex = startIndex + productsPerPage;
-  const currentProducts = products.slice(startIndex, endIndex);
-
-  // Generate an array of page numbers
-  const pageNumbers = [];
-  for (let i = 1; i <= totalPages; i++) {
-    pageNumbers.push(i);
-  }
-
+const ProductList = ({ products, loadMore }) => {
   return (
     <div className="flex flex-wrap justify-around p-4">
-      {currentProducts.map(product => (
+      {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
       <div className="flex justify-center w-full mt-4">
-        {pageNumbers.map(page => (
-          <button
-            key={page}
-            onClick={() => onPageChange(page)}
-            className={`px-3 py-1 mx-1 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none ${currentPage === page ? 'bg-blue-600' : ''}`}
-          >
-            {page}
-          </button>
-        ))}
+        <button
+          onClick={loadMore}
+          className="px-4 py-2 mx-1 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none"
+        >
+          Load More
+        </button>
       </div>
     </div>
   );
 };
 
 export default ProductList;
-
 
 
 
