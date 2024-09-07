56141d9cdf59587811d717d4b59a48f55c6e6a64
tobiloba <tobiosoba777@gmail.com>
2024-09-07 15:14:42 -0700

    Updated

--- apiService.js
+++ apiService.js
@@ -17,9 +17,10 @@ export const fetchData = async (endpoint) => {
   const useEndpoint = endpoint === "/" ? resourceUrl : resourceUrl + endpoint;
   try {
     // const response = await axios.get(${useEndpoint}?organization_id=${import.meta.env.VITE_ORG_ID}&Appid=${import.meta.env.VITE_APP_ID}&Apikey=${import.meta.env.VITE_API_KEY})
-    // "https://timbu-get-all-products.reavdev.workers.dev/3902678375224b58bee75c5b54ff8d0a?organization_id=ca7b597cebd049f6bc533f71d63661be&reverse_sort=false&page=1&size=10&Appid=2JKQ8NFUUQSV2LQ&Apikey=bb65a8a2ce1f4ab2b9b2f144bbecc96620240713125415418473"
-    const urll = "https://timbu-get-all-products.reavdev.workers.dev/3902678375224b58bee75c5b54ff8d0a?organization_id=ca7b597cebd049f6bc533f71d63661be&reverse_sort=false&page=1&size=10&Appid=2JKQ8NFUUQSV2LQ&Apikey=bb65a8a2ce1f4ab2b9b2f144bbecc96620240713125415418473"
-    const response = await axios.get(urll)
+    // const urll = "https://timbu-get-all-products.reavdev.workers.dev/3902678375224b58bee75c5b54ff8d0a?organization_id=ca7b597cebd049f6bc533f71d63661be&reverse_sort=false&page=1&size=10&Appid=2JKQ8NFUUQSV2LQ&Apikey=bb65a8a2ce1f4ab2b9b2f144bbecc96620240713125415418473"
+    // const urll = "https://timbu-get-all-products.reavdev.workers.dev/3902678375224b58bee75c5b54ff8d0a?organization_id=ca7b597cebd049f6bc533f71d63661be&reverse_sort=false&page=1&size=10&Appid=2JKQ8NFUUQSV2LQ&Apikey=bb65a8a2ce1f4ab2b9b2f144bbecc96620240713125415418473"
+    // const response = await axios.get(urll)
+    const response = await axios.get(useEndpoint)
     return response.data;
   } catch (error) {
     console.error('Error fetching data', error);
